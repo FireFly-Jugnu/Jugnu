1d78485e2b07dbbc1caa00c5843cb807b8213240
Varun Verma <varunverma@varunverma.org>
2021-11-26 16:38:13 +0530

    export firebase admin from jugnu

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import * as HF from './HelperFunctions';
 import * as MyTypes from './Types';
+import * as G from './Global';
 
 export namespace jugnu {
     export import createFirebaseCollection = HF.createFirebaseCollection;
     export import initialize = HF.initialize;
+    export import firebaseAdmin = G.firebaseAdmin;
     export import Types = MyTypes;
 }
 
